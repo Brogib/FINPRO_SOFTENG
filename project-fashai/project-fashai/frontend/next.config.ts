b2559344edf8d7ded595b047f4750a5c3aa51ec4
Risyadali <muhammadrisyadali@gmail.com>
2026-05-21 17:15:57 +0700

    icad pertama

--- project-fashai/project-fashai/frontend/next.config.ts
+++ project-fashai/project-fashai/frontend/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "fghpahztpsyidvfrkcfg.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
